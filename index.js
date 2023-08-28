7e8c0ce56c629f2f952903373198d0cd5acc774b
Roderick <Roderickj800@gmail.com>
2023-08-27 21:01:47 -0400

    brought up

---
+++ index.js
@@ -0,0 +1,9 @@
+// First Vowel
+// Write a function firstVowel(str) that takes in a string and returns
+// the first vowel that appears sequentially in the string. If the string does not
+// contain a vowel, return null.
+
+function firstVowel(str) {
+    if()
+}
+///
